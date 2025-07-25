780bbe3af08beeb75ba91d986d43f38675d6e7f7
nomdn <2529897207@qq.com>
2025-07-25 19:49:25 +0800

    0.1.14

--- nonebot_plugin_simple_setu/__init__.py
+++ nonebot_plugin_simple_setu/__init__.py
@@ -1,8 +1,11 @@
+from json import JSONDecodeError
 from pathlib import Path
 import json
+
 import nonebot
 from nonebot import get_plugin_config
-from nonebot.adapters.onebot.v11 import MessageSegment, MessageEvent
+from nonebot.adapters.onebot.v11 import MessageSegment, MessageEvent, ActionFailed
+from nonebot.exception import MatcherException
 from nonebot.plugin import PluginMetadata
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
@@ -38,68 +41,98 @@ async def handle_function(event: MessageEvent, args: Message = CommandArg()):
         if tag := args.extract_plain_text():
             sender_qq = event.get_user_id()
             # 使用 httpx 替换 requests
-            response = await http_client.get(f"https://api.lolicon.app/setu/v2?tag={tag}")
-            json_dict = response.json()
-            title = json_dict["data"][0]["title"]
-            pid = json_dict["data"][0]["pid"]
-            author = json_dict["data"][0]["author"]
-            url = json_dict["data"][0]["urls"]["original"]
-            await setu.send(MessageSegment.at(sender_qq)+f"\n标题:{title}\nPID:{pid}\n作者:{author}")
-            await setu.finish(MessageSegment.image(f"{url}"))
+            try:
+                response = await http_client.get(f"https://api.lolicon.app/setu/v2?tag={tag}")
+                json_dict = response.json()
+                title = json_dict["data"][0]["title"]
+                pid = json_dict["data"][0]["pid"]
+                author = json_dict["data"][0]["author"]
+                url = json_dict["data"][0]["urls"]["original"]
+                await setu.send(MessageSegment.at(sender_qq)+f"\n标题:{title}\nPID:{pid}\n作者:{author}")
+                await setu.finish(MessageSegment.image(f"{url}"))
+            except MatcherException:
+                raise
+            except Exception as e:
+                await setu.finish(f"发生错误：{e}")
         else:
-            sender_qq = event.get_user_id()
-            response = await http_client.get(f"https://api.lolicon.app/setu/v2")
-            json_dict = response.json()
-            title = json_dict["data"][0]["title"]
-            pid = json_dict["data"][0]["pid"]
-            author = json_dict["data"][0]["author"]
-            url = json_dict["data"][0]["urls"]["original"]
-            await setu.send(MessageSegment.at(sender_qq) + f"\n标题:{title}\nPID:{pid}\n作者:{author}")
-            await setu.finish(MessageSegment.image(f"{url}"))
+            try:
+                sender_qq = event.get_user_id()
+                response = await http_client.get(f"https://api.lolicon.app/setu/v2")
+                json_dict = response.json()
+                title = json_dict["data"][0]["title"]
+                pid = json_dict["data"][0]["pid"]
+                author = json_dict["data"][0]["author"]
+                url = json_dict["data"][0]["urls"]["original"]
+                await setu.send(MessageSegment.at(sender_qq) + f"\n标题:{title}\nPID:{pid}\n作者:{author}")
+                await setu.finish(MessageSegment.image(f"{url}"))
+            except MatcherException:
+                raise
+            except Exception as e:
+                await setu.finish(f"发生错误：{e}")
     elif config.api_url == 1:
         if tag := args.extract_plain_text():
-            sender_qq = event.get_user_id()
-            response = await http_client.get(f"https://image.anosu.top/pixiv/json?keyword={tag}")
-            json_dict = response.json()
-            title = json_dict[0]["title"]
-            pid = json_dict[0]["pid"]
-            author = json_dict[0]["user"]
-            url = json_dict[0]["url"]
-            await setu.send(MessageSegment.at(sender_qq)+f"\n标题:{title}\nPID:{pid}\n作者:{author}")
-            await setu.finish(MessageSegment.image(f"{url}"))
+            try:
+                sender_qq = event.get_user_id()
+                response = await http_client.get(f"https://image.anosu.top/pixiv/json?keyword={tag}")
+                json_dict = response.json()
+                title = json_dict[0]["title"]
+                pid = json_dict[0]["pid"]
+                author = json_dict[0]["user"]
+                url = json_dict[0]["url"]
+                await setu.send(MessageSegment.at(sender_qq)+f"\n标题:{title}\nPID:{pid}\n作者:{author}")
+                await setu.finish(MessageSegment.image(f"{url}"))
+            except MatcherException:
+                raise
+            except Exception as e:
+                await setu.finish(f"发生错误：{e}")
         else:
-            sender_qq = event.get_user_id()
-            response = await http_client.get("https://image.anosu.top/pixiv/json")
-            json_dict = response.json()  # 注意：这里可能需要异常处理
-            title = json_dict[0]["title"]
-            pid = json_dict[0]["pid"]
-            author = json_dict[0]["user"]
-            url = json_dict[0]["url"]
-            await setu.send(MessageSegment.at(sender_qq) + f"\n标题:{title}\nPID:{pid}\n作者:{author}")
-            await setu.finish(MessageSegment.image(f"{url}"))
+            try:
+                sender_qq = event.get_user_id()
+                response = await http_client.get("https://image.anosu.top/pixiv/json")
+                json_dict = response.json()  # 注意：这里可能需要异常处理
+                title = json_dict[0]["title"]
+                pid = json_dict[0]["pid"]
+                author = json_dict[0]["user"]
+                url = json_dict[0]["url"]
+                await setu.send(MessageSegment.at(sender_qq) + f"\n标题:{title}\nPID:{pid}\n作者:{author}")
+                await setu.finish(MessageSegment.image(f"{url}"))
+            except MatcherException:
+                raise
+            except Exception as e:
+                await setu.finish(f"发生错误：{e}")
 
 leg = on_command("leg", aliases={"腿子", "来份腿子"})
 
 @leg.handle()
 async def handle_function(event: MessageEvent, args: Message = CommandArg()):
     # 提取参数纯文本作为地名，并判断是否有效
-    sender_qq_leg = event.get_user_id()
-    response_leg = await http_client.get("https://api.lolimi.cn/API/meizi/api.php?type=json")
-    json_dict_leg = response_leg.json()
-    image_leg = json_dict_leg["text"]
-    at_segment_leg = MessageSegment.at(user_id=sender_qq_leg)
-    await setu.send(at_segment_leg)
-    await setu.finish(MessageSegment.image(image_leg))
+    try:
+        sender_qq_leg = event.get_user_id()
+        response_leg = await http_client.get("https://api.lolimi.cn/API/meizi/api.php?type=json")
+        json_dict_leg = response_leg.json()
+        image_leg = json_dict_leg["text"]
+        at_segment_leg = MessageSegment.at(user_id=sender_qq_leg)
+        await setu.send(at_segment_leg)
+        await setu.finish(MessageSegment.image(image_leg))
+    except MatcherException:
+        raise
+    except Exception as e:
+        await leg.finish(f"发生错误：{e}")
 
 girl = on_command("girl", aliases={"少女写真", "来份写真"})
 
 @girl.handle()
 async def handle_function(event: MessageEvent, args: Message = CommandArg()):
     # 提取参数纯文本作为地名，并判断是否有效
-    sender_qq_girl = event.get_user_id()
-    response_girl = await http_client.get("https://api.lolimi.cn/API/meinv/api.php?type=json")
-    json_dict_girl = response_girl.json()
-    image_girl = json_dict_girl["data"]["image"]
-    at_segment_girl = MessageSegment.at(user_id=sender_qq_girl)
-    await setu.send(at_segment_girl)
-    await setu.finish(MessageSegment.image(image_girl))
+    try:
+        sender_qq_girl = event.get_user_id()
+        response_girl = await http_client.get("https://api.lolimi.cn/API/meinv/api.php?type=json")
+        json_dict_girl = response_girl.json()
+        image_girl = json_dict_girl["data"]["image"]
+        at_segment_girl = MessageSegment.at(user_id=sender_qq_girl)
+        await girl.send(at_segment_girl)
+        await girl.finish(MessageSegment.image(image_girl))
+    except MatcherException:
+        raise
+    except Exception as e:
+        await girl.finish(f"发生错误：{e}")
